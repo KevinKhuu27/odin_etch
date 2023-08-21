72a1510ae1fef5a7c224c1239ef3ab9ee6a0f9b1
KevinKhuu27 <khuu.kevin27@hotmail.com>
2023-08-20 22:06:20 -0400

    Added the change size button
    The user can now click the button to change the size of the grid. The grid should change accordingly, using up the same amount of space as before.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const container = document.querySelector('.container');
 
+function makeSizeBtn ()
+{
+    const btnDiv = document.createElement('div');
+    btnDiv.classList.add('btnDiv');
+
+    const sizeBtn = document.createElement('btn');
+    sizeBtn.addEventListener('click', () => {
+        clearGrid();
+        createGrid(getSize());
+    })
+    sizeBtn.textContent = "Change Size!";
+
+    btnDiv.appendChild(sizeBtn);
+    container.appendChild(btnDiv);
+}
+
+function clearGrid ()
+{
+    let rows = document.querySelectorAll('.row');
+    rows.forEach((row) => {
+        container.removeChild(row);
+    }) 
+}
+
 function createGrid (size)
 {
     for (var i = 0; i < size; i++)
@@ -22,5 +46,15 @@ function createGrid (size)
     }
 }
 
+function getSize () 
+{
+    let size = -1;
+    while (size < 0 || size > 100)
+    {
+        size = prompt("Size of grid (Max 100): ");
+    }    
+    return size;
+}
 
+makeSizeBtn();
 createGrid(32);
